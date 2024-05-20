dec8dc53ef0fccc5df7a6ec49902a88a9a1ede09
Shashwat <59992419+shashwatthapa@users.noreply.github.com>
2024-05-20 21:03:12 +0545

    Update script.js

--- password generator/script.js
+++ password generator/script.js
@@ -1,3 +1,4 @@
+const btn = document.querySelector('button');
 const slider = document.querySelector('#num');
 const length = document.querySelector("#length");
 const upperCase = document.querySelector('#uppercase');
@@ -11,21 +12,47 @@ function updateSlider(){
 slider.addEventListener('input',updateSlider);
 function generateCapital(){
 	a = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-	num = math.floor(math.random()*25);
+	num = Math.floor(Math.random()*25);
 	return a[num];
 }
 function generateLower(){
 	a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-	num = math.floor(math.random()*25);
+	num = Math.floor(Math.random()*25);
 	return a[num];
 }
 function generateNumber(){
 	a = [1,2,3,4,5,6,7,8,9,10];
-	num = math.floor(math.random()*9);
+	num = Math.floor(Math.random()*9);
 	return a[num];
 }
 function generateSymbol(){
 	a = ['@','$','%','&','£'];
-	num = math.floor(math.random()*4);
+	num = Math.floor(Math.random()*4);
 	return a[num];
 }
+function generatePassword()
+{
+	let password = '';
+
+	for (let i = 0;i<slider.value;i++){
+		if (symbol.checked==true){
+			password = password + generateSymbol();
+		}
+		
+		if (upperCase.checked==true){
+			password = password + generateCapital();
+		}
+		if (lowerCase.checked==true){
+			password = password + generateLower();
+		}
+		if (number.checked==true){
+			password = password + generateNumber();
+		}
+		
+	}
+	return password;
+}
+btn.addEventListener('click',()=>{
+	console.log(generatePassword());
+})
+
