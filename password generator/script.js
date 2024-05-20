e6e73e452485290178a5a3749215d1112af86f47
Shashwat <59992419+shashwatthapa@users.noreply.github.com>
2024-05-20 21:05:04 +0545

    Update script.js

--- password generator/script.js
+++ password generator/script.js
@@ -6,30 +6,49 @@ const lowerCase = document.querySelector('#lowercase');
 const number = document.querySelector('#number');
 const symbol = document.querySelector('#symbol');
 let c = '';
-function updateSlider(){
+
+
+function updateSlider()
+{
 	length.innerText ='Length: '+ slider.value;
 }
+
+
 slider.addEventListener('input',updateSlider);
-function generateCapital(){
+
+
+function generateCapital()
+{
 	a = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 	num = Math.floor(Math.random()*25);
 	return a[num];
 }
-function generateLower(){
+
+
+function generateLower()
+{
 	a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 	num = Math.floor(Math.random()*25);
 	return a[num];
 }
-function generateNumber(){
+
+
+function generateNumber()
+{
 	a = [1,2,3,4,5,6,7,8,9,10];
 	num = Math.floor(Math.random()*9);
 	return a[num];
 }
-function generateSymbol(){
+
+
+function generateSymbol()
+{
 	a = ['@','$','%','&','£'];
 	num = Math.floor(Math.random()*4);
 	return a[num];
 }
+
+
 function generatePassword()
 {
 	let password = '';
@@ -52,7 +71,10 @@ function generatePassword()
 	}
 	return password;
 }
-btn.addEventListener('click',()=>{
+
+
+btn.addEventListener('click',()=>
+	{
 	console.log(generatePassword());
 })
 
