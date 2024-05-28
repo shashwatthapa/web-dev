5fe35e0d2b9eedcc9bfe70cd4230d6ee981ad59d
Shashwat <59992419+shashwatthapa@users.noreply.github.com>
2024-05-28 21:56:19 +0545

    Update script.js

--- currency converter/script.js
+++ currency converter/script.js
@@ -1,1 +1,5 @@
 var btn = document.querySelector('button');
+var input = document.querySelector('input');
+var value = document.querySelector('select');
+btn.addEventListener('click',()={}
+                    )
